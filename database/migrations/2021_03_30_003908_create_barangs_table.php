bd09c9bccecdf243b4914bde762e15b9cf8c9ce0
= <=>
2021-03-30 07:52:30 +0700

    membuat model,migration, seeder

--- database/migrations/2021_03_30_003908_create_barangs_table.php
+++ database/migrations/2021_03_30_003908_create_barangs_table.php
@@ -14,7 +14,12 @@ class CreateBarangsTable extends Migration
     public function up()
     {
         Schema::create('barangs', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id_barang');
+            $table->string('kode_barang')->unique();
+            $table->string('nama_barang');
+            $table->string('kategori_barang');
+            $table->integer('harga');
+            $table->integer('qty');
             $table->timestamps();
         });
     }
